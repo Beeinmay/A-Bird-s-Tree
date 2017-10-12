631f1142c7a6758d7950fa873563c090e741b2ae
Beeinmay <31043341+Beeinmay@users.noreply.github.com>
2017-10-12 18:03:54 -0400

    Update home.php

--- home.php
+++ home.php
@@ -3,6 +3,7 @@
 <html>
 <head>
     <title>Jerry</title>
+    //An incredibly underwhelming website soon to be filled with material from MAT223.
     <link rel="stylesheet" href="jerryHome.css">
 </head>
 
