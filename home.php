24cdc2e992997b09b9724497ea16cf256d62aa98
Beeinmay <31043341+Beeinmay@users.noreply.github.com>
2017-10-17 23:41:45 -0400

    Update home.php

--- home.php
+++ home.php
@@ -10,7 +10,7 @@
 	<?php include "navigation.php"; ?>
 	
 	<h2>A Quick Summary of AST101 Midterm 2013</h2>
-	<h4>Focusing on the areas that need correction first.</h4>
+	<h3>Focusing on the areas that need correction first.</h3>
 	<ol>
 		<li>
 		Which of the following is FALSE?
@@ -37,7 +37,7 @@
 		<li>
 			Neptune is cold (about -200 C). To the naked eye, Neptune is blue in colour. Which of the
 			following could correspond to the spectrum of light EMITTED by Neptune?
-			<p><img src="images/ast101 midterm 2013 q3.jpg" width="522" height="308"></p>
+			<p><img src="images/ast101 midterm 2013 q3.jpg" width="522" height="308" alt="*Missing photo. Description: Various graphs on the light spectrum."></p>
 			<p>Answer: (a). [This answer may require elaboration.] Notice that all of the waves, save wave A, reach a somewhat high intensity.</p>
 			<p><a href="http://coolcosmos.ipac.caltech.edu/ask/138-Why-is-Neptune-blue-">Furthermore,</a> Neptune's atmosphere is made up of hydrogen, helium and methane. The methane in Neptune's upper atmosphere absorbs the red light from the sun but reflects the blue light from the Sun back into space. This is why Neptune appears blue.</p>
 		</li>
@@ -53,7 +53,52 @@
 			<p>Answer: (a). All planets ORBIT in the SAME direction; some ROTATE in DIFFERENT directions.</p>
 			<p>Recall that <a href="https://en.wikipedia.org/wiki/Venus">Venus</a> rotates clockwise in retrograde rotation once every 243 Earth days, and that <a href="https://io9.gizmodo.com/5847495/how-uranus-got-knocked-on-its-side">Uranus's</a> spin axis lies 98 degrees off of its orbital plane with the Sun (sideways).</p>
 		</li>
+		<li>
+			A star is moving away from an observer on Earth. What can the observer say about the
+			wavelengths of the spectral lines of hydrogen that they will detect in the spectrum of the star,
+			compared to those they would detect from a sample of hydrogen at rest in a laboratory on Earth?
+			<ul>
+				<li>The spectral lines of atoms are like chemical fingerprints and thus they will remain at the same wavelength.</li>
+				<li>The spacings between the spectral lines will be different and the lines will be at smaller wavelengths.</li>
+				<li>The spacings between the spectral lines will be different and the lines will be shifted towards the red.</li>
+				<li> The spacings between the spectral lines will remain the same but they will appear at larger wavelengths. </li>
+				<li> The spectral lines would change from absorption to emission lines.</li>
+			</ul>
+			<p>Answer: (d).</p>
+			<p>*Need to look into this section.</p>
+			<p>A <a href="https://en.wikipedia.org/wiki/Spectral_line">spectral line</a> is a dark or bright line in an otherwise uniform and continuous spectrum, resulting from emission or absorption of light in a narrow frequency range, compared with the nearby frequencies.</p>
+		</li>		
 	</ol>
-	
+	<h4>21. Match the phases with the descriptions below. You may use any of the following phases: FULL, NEW, FIRST QUARTER, or THIRD QUARTER.</h4>
+	<table>
+		<tr>
+			<th>DESCRIPTION</td><th>PHASE</th>
+		</tr>
+		<tr>
+			<td>Phase of the Moon that rises at noon</td>
+			<td>*FIRST QUARTER</td>
+		</tr>
+		<tr>
+			<td>Phase of the Moon that rises at sunset</td>
+			<td>FULL</td>
+		</tr>
+		<tr>
+			<td>Phase of the Moon during a solar eclipse </td>
+			<td>NEW</td>
+		</tr>
+		<tr>
+			<td>Phase of the Moon that is high overhead at noon </td>
+			<td>*NEW</td>
+		</tr>
+		<tr>
+			<td>Phase of the Moon during a lunar eclipse</td>
+			<td>FULL</td>
+		</tr>
+		<tr>
+			<td>Phase of the EARTH as seen from the Moon during a solar eclipse</td>
+			<td>FULL</td>
+		</tr>
+	</table>
+	<p>* = would benefit from more review</p>
 </body>
 </html>
