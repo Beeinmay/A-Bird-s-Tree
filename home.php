a2bb8b2bae98909140b63fe028e02aa9a04a039a
Beeinmay <31043341+Beeinmay@users.noreply.github.com>
2017-10-17 22:06:28 -0400

    Update home.php

--- home.php
+++ home.php
@@ -2,14 +2,37 @@
 
 <html>
 <head>
-    <title>Jerry</title>
-    //An incredibly underwhelming website soon to be filled with material from MAT223.
-    <link rel="stylesheet" href="jerryHome.css">
+	<title>Jerry the Website</title>
+	<link rel="stylesheet" href="css_home.css"> 
 </head>
 
-
 <body>
-    <h1>Hello World! I'm Jerry.</h1>
+	<?php include "navigation.php"; ?>
+	
+	//AST101 Study Site
+	
+	<h2>A Quick Summary of AST101 Midterm 2013</h2>
+	<ol>
+		<li>
+		Which of the following is FALSE?
+			<ul>
+				<li>Our solar system contains many planets</li>
+				<li>Our galaxy contains many solar systems</li>
+				<li>Our universe contains many galaxies</li>
+				<li>Our galaxy contains many stars</li>
+				<li>Our solar system contains many stars</li>
+			</ul>
+			<ul>
+				<li>Answer: (e) is FALSE. There is only one star in our solar system--the sun!</li>
+			</ul>
+		</li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ol>
+	
 </body>
-
 </html>
