bd82651f831b4455c9d6a84977fe34a09ad2c95e
Beeinmay <31043341+Beeinmay@users.noreply.github.com>
2017-10-17 22:56:48 -0400

    Update home.php

--- home.php
+++ home.php
@@ -10,6 +10,7 @@
 	<?php include "navigation.php"; ?>
 	
 	<h2>A Quick Summary of AST101 Midterm 2013</h2>
+	<h5>Focusing on the areas that need correction first.</h5>
 	<ol>
 		<li>
 		Which of the following is FALSE?
@@ -22,12 +23,26 @@
 			</ul>
 			<p>Answer: (e) is FALSE. There is only one star in our solar system--the sun!</p>
 		</li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
+		<li>
+			Imagine you are an alien living on a planet 50 light from Earth.
+			You use a very, very powerful telescope to look at Earth.
+			If you look at Earth today, you would see Earth the way it did, does, or will look in the year:
+			<ul>
+				<li>1913</li>
+				<li>1963</li>
+				<li>1988</li>
+				<li>2013</li>
+				<li>2063</li>
+			</ul>
+			<p>Answer: (b). (Today) - (the time it takes for the light to travel from Earth to the alien planet) = 2013 - 50 = 1963.</p>
+		</li>
+		<li>
+			Neptune is cold (about -200 C). To the naked eye, Neptune is blue in colour. Which of the
+			following could correspond to the spectrum of light EMITTED by Neptune?
+			<p><img src="images/ast101 midterm 2013 q3.jpg" width="522" height="308"></p>
+			<p>Answer: (a). [This answer may require elaboration.] Notice that all of the waves, save wave A, reach a somewhat high intensity.</p>
+			<p><a href="http://coolcosmos.ipac.caltech.edu/ask/138-Why-is-Neptune-blue-">Furthermore,</a> Neptune's atmosphere is made up of hydrogen, helium and methane. The methane in Neptune's upper atmosphere absorbs the red light from the sun but reflects the blue light from the Sun back into space. This is why Neptune appears blue.</p>
+		</li>
 	</ol>
 	
 </body>
