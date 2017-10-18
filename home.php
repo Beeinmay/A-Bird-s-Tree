21ad45afb9afd4ede0843f88ab3ea3e7a9051d9e
Beeinmay <31043341+Beeinmay@users.noreply.github.com>
2017-10-17 23:15:29 -0400

    Update home.php

--- home.php
+++ home.php
@@ -10,7 +10,7 @@
 	<?php include "navigation.php"; ?>
 	
 	<h2>A Quick Summary of AST101 Midterm 2013</h2>
-	<h5>Focusing on the areas that need correction first.</h5>
+	<h4>Focusing on the areas that need correction first.</h4>
 	<ol>
 		<li>
 		Which of the following is FALSE?
@@ -24,9 +24,7 @@
 			<p>Answer: (e) is FALSE. There is only one star in our solar system--the sun!</p>
 		</li>
 		<li>
-			Imagine you are an alien living on a planet 50 light from Earth.
-			You use a very, very powerful telescope to look at Earth.
-			If you look at Earth today, you would see Earth the way it did, does, or will look in the year:
+			Imagine you are an alien living on a planet 50 light from Earth. You use a very, very powerful telescope to look at Earth. If you look at Earth today, you would see Earth the way it did, does, or will look in the year:
 			<ul>
 				<li>1913</li>
 				<li>1963</li>
@@ -42,6 +40,18 @@
 			<p><img src="images/ast101 midterm 2013 q3.jpg" width="522" height="308"></p>
 			<p>Answer: (a). [This answer may require elaboration.] Notice that all of the waves, save wave A, reach a somewhat high intensity.</p>
 			<p><a href="http://coolcosmos.ipac.caltech.edu/ask/138-Why-is-Neptune-blue-">Furthermore,</a> Neptune's atmosphere is made up of hydrogen, helium and methane. The methane in Neptune's upper atmosphere absorbs the red light from the sun but reflects the blue light from the Sun back into space. This is why Neptune appears blue.</p>
+		</li>
+		<li>
+			Which of the following best describes the motion of planets in the solar system?
+			<ul>
+				<li>All planets orbit in the same direction; some rotate in different directions. </li>
+				<li>All planets orbit and rotate in the same direction.</li>
+				<li>Planets orbit in different directions, but they all rotate in the same direction</li>
+				<li>Planets orbit and rotate in all different directions.</li>
+				<li>There is no general pattern to the motions of planets in our solar system.</li>
+			</ul>
+			<p>Answer: (a). All planets ORBIT in the SAME direction; some ROTATE in DIFFERENT directions.</p>
+			<p>Recall that <a href="https://en.wikipedia.org/wiki/Venus">Venus</a> rotates clockwise in retrograde rotation once every 243 Earth days, and that <a href="https://io9.gizmodo.com/5847495/how-uranus-got-knocked-on-its-side">Uranus's</a> spin axis lies 98 degrees off of its orbital plane with the Sun (sideways).</p>
 		</li>
 	</ol>
 	
